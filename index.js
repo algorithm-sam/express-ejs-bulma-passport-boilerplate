54c054fbd722c84f80f7d37c77e3246490fa5a97
unknown <oluwatobisamuel2020@gmail.com>
2018-12-24 23:45:19 +0000

    set up express-sessions

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session');
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+//set up sessions
+
+app.use(session({
+    saveUninitialized: false,
+    resave: true,
+    secret: 'some-secret'
+}));
+
+
+//set up any other middleware in here
+
+
+// set up view engines
+
 
 app.set('view engine', 'ejs');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index')
