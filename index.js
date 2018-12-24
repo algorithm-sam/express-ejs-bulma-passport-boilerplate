850b250685ad46b3f67dc6d2273aea34ad320cc7
unknown <oluwatobisamuel2020@gmail.com>
2018-12-24 23:26:20 +0000

    setup views and static assets

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 // console.log(ENV);
 app.listen(process.env.port || 3000, () => {
